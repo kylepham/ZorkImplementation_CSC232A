d18cc3b624867268b3646d273ee7b377f13db53c
kylepham <hoanghuypham2121@gmail.com>
2020-03-30 17:51:07 +0000

    generate location class

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Item it = new Item("Sandwich", "Food", "A peanut butter and jelly sandwich");
+        it.setType("Shit");
+        System.out.println(it.toString());
+        Location m = new Location("House", "Pikachu");
+        System.out.println(m.getNameLocal());
+        m.addItem("Pikachu");
+        m.addItem("Ash");
+        m.addItem("Bê đê");
+        System.out.println(m.removeItem("Pikachu"));
     }
 }
-// h bắt đầu làm lun à?
